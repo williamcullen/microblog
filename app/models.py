888e2bfd7a2cc32ef497ba410a00ffe6f537b9de
williamcullen <williamcullen@foxmail.com>
2017-07-14 09:23:08 +0800

    全局搜索

--- app/models.py
+++ app/models.py
@@ -1,12 +1,22 @@
 # _*_ encoding:utf-8 _*_
-__author__ = 'williamcullen'
-__date__ = '2017/7/9 16:02'
 
-from app import db
+from app import db, app
+from hashlib import md5
+import sys
 
+if sys.version_info >= (3, 0):
+    enable_search = False
+else:
+    enable_search = True
+    import flask_whooshalchemy as whooshalchemy
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
+
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
 
 
 class User(db.Model):
@@ -15,15 +25,25 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     role = db.Column(db.Integer, default=ROLE_USER)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
-    # def is_authenticated(self):
-    #     return True
+    # is_authenticated = True
 
-    is_authenticated = True
+    def is_authenticated(self):
+        return True
 
-    is_active = True
+    def is_active(self):
+        return True
 
-    is_anonymous = True
+    def is_anonymous(self):
+        return True
 
     def get_id(self):
         try:
@@ -31,11 +51,44 @@ class User(db.Model):
         except NameError:
             return str(self.id)  # python 3
 
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() == None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=new_nickname).first() == None:
+                break
+            version += 1
+        return new_nickname
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
 
 class Post(db.Model):
+    __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
@@ -43,3 +96,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+
+if enable_search:
+    whooshalchemy.whoosh_index(app, Post)
